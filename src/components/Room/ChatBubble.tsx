932427ad08df6e1764a95a072d396e1859185134
Juliard Actub <56096375+codeMonkey20@users.noreply.github.com>
2023-05-09 15:00:14 +0800

    chat bubble word wrapping

--- src/components/Room/ChatBubble.tsx
+++ src/components/Room/ChatBubble.tsx
@@ -9,7 +9,7 @@ interface Props {
 const ChatBubble = ({ children, position, self }: Props) => {
   return (
     <div className={`py-2 px-3 w-fit bubble-${position}${self ? "-self" : ""}`}>
-      <p className={"max-w-lg"}>{children}</p>
+      <p className={"max-w-lg break-words"}>{children}</p>
     </div>
   );
 };
