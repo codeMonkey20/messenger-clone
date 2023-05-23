ec8804a340233ccb5ce8e7329b3563f457aa01d9
Juliard Actub <56096375+codeMonkey20@users.noreply.github.com>
2023-05-23 15:54:44 +0800

    new socket configuration

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,10 +1,10 @@
 import { io } from "socket.io-client";
 
 const socketURL =
-  process.env.NODE_ENV === "development" ? "wss://localhost:8000" : "wss://messenger-clone-socket.herokuapp.com";
+  process.env.NODE_ENV === "development" ? "ws://localhost:8000" : "ws://messenger-clone-socket.herokuapp.com";
 const socket = io(socketURL, {
   autoConnect: false,
   forceNew: true,
-  withCredentials: true,
+  withCredentials: false,
 });
 export default socket;
