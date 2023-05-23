3b9a260cdcf7e45427985a8d4af702a35baf4342
Juliard Actub <56096375+codeMonkey20@users.noreply.github.com>
2023-05-23 16:12:06 +0800

    socket uri fix

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const socketURL =
-  process.env.NODE_ENV === "development" ? "ws://localhost:8000" : "ws://messenger-clone-socket.herokuapp.com";
+  process.env.NODE_ENV === "development" ? "ws://localhost:8000" : "wss://messenger-clone-socket.herokuapp.com";
 const socket = io(socketURL, {
   autoConnect: false,
   forceNew: true,
